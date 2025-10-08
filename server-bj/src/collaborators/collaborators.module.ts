7f50b48e36299541ad26caa2c99db35e10f2e5c8
Iván Carrillo <ivancz.com@gmail.com>
2025-10-07 18:29:06 -0600

    Funciona otra vez

--- server-bj/src/collaborators/collaborators.module.ts
+++ server-bj/src/collaborators/collaborators.module.ts
@@ -3,10 +3,15 @@ import { CollaboratorsService } from './collaborators.service';
 import { CollaboratorsController } from './collaborators.controller';
 import { Collaborator } from './entities/collaborator.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoriesModule } from 'src/categories/categories.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Collaborator])],
+  imports: [
+    TypeOrmModule.forFeature([Collaborator]),
+    CategoriesModule
+],
   controllers: [CollaboratorsController],
   providers: [CollaboratorsService],
+  exports: [TypeOrmModule]
 })
 export class CollaboratorsModule {}
--- server-bj/src/promotions/promotions.module.ts
+++ server-bj/src/promotions/promotions.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PromotionsService } from './promotions.service';
 import { PromotionsController } from './promotions.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Promotion } from './entities/promotion.entity';
+import { CollaboratorsModule } from 'src/collaborators/collaborators.module';
 
 @Module({
   controllers: [PromotionsController],
   providers: [PromotionsService],
+  imports: [TypeOrmModule.forFeature([Promotion]), CollaboratorsModule],
+  exports: [TypeOrmModule]
 })
 export class PromotionsModule {}
