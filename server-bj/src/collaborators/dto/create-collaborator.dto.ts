4fee4b367e28433419ff07fc53c9380a3a9f2e8d
A01751315 <a01751315@tec.mx>
2025-10-09 23:01:08 -0600

    fix
    fix

--- server-bj/src/collaborators/dto/create-collaborator.dto.ts
+++ server-bj/src/collaborators/dto/create-collaborator.dto.ts
@@ -51,7 +51,7 @@ export class CreateCollaboratorDto {
   // 👇 Multiple category IDs
   @IsArray()
   @ArrayNotEmpty()
-  @IsInt({ each: true })s
+  @IsInt({ each: true })
   categoryIds: number[];
 
   @IsOptional()
