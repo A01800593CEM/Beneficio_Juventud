1c8b51f01e8b74a2ea51684c6c8039278dae8eb2
A01751347 <149411831+A01751347@users.noreply.github.com>
2025-10-15 19:54:01 -0600

    probar_notis

--- server-bj/src/notifications/notifications.module.ts
+++ server-bj/src/notifications/notifications.module.ts
@@ -4,6 +4,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PromotionsModule } from 'src/promotions/promotions.module';
 import admin from "firebase-admin";
 import { Promotion } from 'src/promotions/entities/promotion.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Favorite } from 'src/favorites/entities/favorite.entity';
+import { Notification } from './entities/notification.entity';
 const serviceAccount = require("../../beneficio-joven-firebase-adminsdk-fbsvc-f6886c6f95.json");
 
 const firebaseApp = admin.initializeApp({
@@ -20,6 +23,6 @@ const firebaseApp = admin.initializeApp({
     }, 
   NotificationsService],
   exports: [NotificationsService, 'FIREBASE_ADMIN'],
-  imports: [TypeOrmModule.forFeature([Promotion]), forwardRef(() => PromotionsModule)],
+  imports: [TypeOrmModule.forFeature([Promotion, User, Favorite, Notification]), forwardRef(() => PromotionsModule)],
 })
 export class NotificationsModule {}
