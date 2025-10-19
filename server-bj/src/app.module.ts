11132aac825e368c18c5f5bc624892febc38e618
A01751315 <a01751315@tec.mx>
2025-10-19 00:58:56 -0600

    API google sin tarjeta
    funciona si el problema no es economico

--- server-bj/src/app.module.ts
+++ server-bj/src/app.module.ts
@@ -17,6 +17,7 @@ import { NotificationsModule } from './notifications/notifications.module';
 //Notificaciones con schedule
 import { ScheduleModule } from '@nestjs/schedule';
 import { ExpirationsModule } from './expirations/expirations.module';
+import { PlacesModule } from './config/places.module';
 
 
 @Module({
@@ -55,7 +56,8 @@ import { ExpirationsModule } from './expirations/expirations.module';
     NotificationsModule,
     //Schedule
     ScheduleModule.forRoot(),
-    ExpirationsModule
+    ExpirationsModule,
+    PlacesModule
   ],
   controllers: [AppController],
   providers: [AppService],
