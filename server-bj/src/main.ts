77034bf151e0e4ebb3588d4d5aa1951de1824460
A01751315 <a01751315@tec.mx>
2025-10-20 21:29:46 -0600

    main server sin certificado

--- server-bj/src/main.ts
+++ server-bj/src/main.ts
@@ -1,27 +1,17 @@
-import * as fs from 'fs';
-import * as path from 'path';
 import { NestFactory } from '@nestjs/core';
 import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { sendNotification } from './enviar';
+import { seedDatabase } from './seedServer'
 
 async function bootstrap() {
-  const certDir = path.join(process.cwd(), 'src', 'certificates');
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(certDir, 'key.pem')),
-    cert: fs.readFileSync(path.join(certDir, 'cert.pem')),
-  };
-
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ https: httpsOptions }),
+    new FastifyAdapter()
   );
-
-  app.enableCors({ origin: true, credentials: true });
-  await app.listen(3000, '0.0.0.0');
-  console.log('✅ HTTPS running at https://localhost:3000');
+  await app.listen(process.env.PORT ?? 3000, "127.0.0.1");
 }
-
 bootstrap();
